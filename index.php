e01f668b82d396ac304d864baf36792f7ea2423a
Mark S Andre <marks@imentor.dk>
2013-06-21 14:35:28 +0200

    Emai Validation: footer added to index.php file

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 <body>
 	<h1> Processing Email validation form </h1>
 	<h1> validate email address </h1>
+	<br />
+	<br />
+	<br />
+	<h4> Link to Google.com | Link to Bitcket.org</h4>	
 </body>
 </html>
